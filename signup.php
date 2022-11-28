b9c95c4614de552174cdcb11b559f72ad16a7924
Vanessa Pereira <pereira.van2001@gmail.com>
2022-11-28 23:24:15 +0000

    Creating the error messages

--- signup.php
+++ signup.php
@@ -4,7 +4,34 @@
 
     <main class="d-flex justify-content-center mt-5">
         <div>
-            <h1>Sign Up</h1>
+            <h1 class="text-center">Sign Up</h1>
+            <?php 
+                if (isset($_GET['error'])) {
+                    if ($_GET['error'] == "emptyfields") {
+                        echo '<p class="text-danger text-center">Fill in all fields!</p>';
+                    }
+                    elseif ($_GET['error'] == "invaliduidmail") {
+                        echo '<p class="text-danger text-center">Invalid username and email!</p>';
+                    }
+                    elseif ($_GET['error'] == "invaliduid") {
+                        echo '<p class="text-danger text-center">Invalid username!</p>';
+                    }
+                    elseif ($_GET['error'] == "invalidemail") {
+                        echo '<p class="text-danger text-center">Invalid email!</p>';
+                    }
+                    elseif ($_GET['error'] == "passwordcheck") {
+                        echo '<p class="text-danger text-center">Your passwords do not match!</p>';
+                    }
+                    elseif ($_GET['error'] == "usertaken") {
+                        echo '<p class="text-danger text-center">Username is already taken!</p>';
+                    }
+                }
+                elseif (isset($_GET['signup'])) {
+                    if ($_GET['signup'] == "success") {
+                        echo '<p class="text-success text-center">Signup successful!</p>';
+                    }
+                }
+            ?>
             <form action="includes/signup.inc.php" method="post">
                 <div class="form-group">
                     <input type="text" name="uid" placeholder="Username" class="form-control">
@@ -18,7 +45,9 @@
                 <div class="form-group">
                     <input type="password" name="pwd-repeat" placeholder="Repeat password" class="form-control">
                 </div>
-                <button type="submit" name="signup-submit" class="btn btn-success">Sign up</button>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" name="signup-submit" class="btn btn-success">Sign up</button>
+                </div>
             </form>
         </div>
     </main>
